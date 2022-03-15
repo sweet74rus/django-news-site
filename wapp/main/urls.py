b97c035bd36af3ecac6248bbf6d5795b7123325d
Вячеслав Бородин <vikc003@mail.ru>
2022-03-15 11:43:48 +0500

    add static and css templates

--- wapp/main/urls.py
+++ wapp/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about),
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
     path('registration', views.registration)
 ]
