d37eac4a6f1e6668ad8c0f546fd617f2849eeae8
Вячеслав Бородин <vikc003@mail.ru>
2022-03-15 21:38:33 +0500

    add models

--- wapp/news/urls.py
+++ wapp/news/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('news/', views.index, name='home'),
-    path('about', views.about, name='about'),
+    path('', views.news_home, name='news-home'),
 ]
--- wapp/news/views.py
+++ wapp/news/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def news_home(request):
-    return render(request, 'main/about.html')
+    data = {
+        'title': 'Новости'
+    }
+    return render(request, 'news/news_home.html', data)
